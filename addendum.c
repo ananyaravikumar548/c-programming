6f9ed2bad86392a755b7568478bfe34512ac1abb
ananyaravikumar548 <ananyaravikumar71@gmail.com>
2024-11-19 11:59:58 +0530

    Create addendum.c

---
+++ addendum.c
@@ -0,0 +1,28 @@
+int main()
+{
+    int array[50], position, i, n, value;
+     printf("Enter number of elements in the array\n");
+    scanf("%d", &n);
+    
+    printf("Enter %d elements\n", n);
+    for (i = 0; i < n; i++)    
+        scanf("%d", &array[i]);
+    
+    printf("Please enter the location where you want to insert an new element\n");
+    scanf("%d", &position);
+    
+    printf("Please enter the value\n");
+    scanf("%d", &value);
+    
+    for (i= n - 1; i>= position - 1; i--)    
+        array[i+1] = array[i];
+    
+    array[position-1] = value;
+    
+    printf("Resultant array is\n");
+    
+    for (i = 0; i<= n; i++)    
+        printf("%d\n", array[i]);    
+    
+    return 0;
+}
